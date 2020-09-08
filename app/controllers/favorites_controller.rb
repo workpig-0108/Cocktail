f19a21015f42f33d1c8778ecf6b075036c185322
vagrant <vagrant@localhost.localdomain>
2020-09-08 10:54:07 +0000

    [start]favorite-js

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,23 @@
 class FavoritesController < ApplicationController
 	def create
-		recipe = Recipe.find(params[:recipe_id])
-	    favorite = current_user.favorites.new(recipe_id: recipe.id)
-	    favorite.save
-	    redirect_to recipe_path(recipe)
+		@recipe = recipe.find(params[:recipe_id])
+    	favorite = @recipe.favorites.new(user_id: current_user.id)
+    	favorite.save
 	end
 
 	def destroy
-		recipe = Recipe.find(params[:recipe_id])
-	    favorite = current_user.favorites.find_by(recipe_id: recipe.id)
-	    favorite.destroy
-	    redirect_to recipe_path(recipe)
+		@recipe = recipe.find(params[:recipe_id])
+    	favorite = current_user.favorites.find_by(recipe_id: @recipe.id)
+    	favorite.destroy
 	end
+
+	private
+  	def redirect
+	    case params[:redirect_id].to_i
+	    when 0
+	      redirect_to recipes_path
+	    when 1
+	      redirect_to recipe_path(@recipe)
+	    end
+  	end
 end
